0b44ea3c5ea830d52505586ed5a435ea2539b653
David Nikolaj Vinje <davv@itu.dk>
2021-12-05 11:31:17 +0100

    Add Day 6

---
+++ Day6.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Day6 {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("day6.txt");
+        part1(file);
+        part2(file);
+    }
+
+    public static void part1(File file) throws FileNotFoundException {
+        Scanner scan = new Scanner(file);
+    }
+
+    public static void part2(File file) throws FileNotFoundException {
+        Scanner scan = new Scanner(file);
+    }
+}
