dfaee421c68926076ca9c590206872c1146886d3
David Nikolaj Vinje <davv@itu.dk>
2021-12-13 17:20:13 +0100

    try something

--- Day13.java
+++ Day13.java
@@ -5,17 +5,15 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Day13 {
+    static int height = 0, width = 0;
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("day13.txt");
-        part1(file);
-        // part2(file);
+        // part1(file);
+        part2(file);
     }
 
     public static void part1(File file) throws FileNotFoundException {
         Scanner scan = new Scanner(file);
-        int width = 0, height = 0;
-        // List<Integer> xs = new ArrayList<>();
-        // List<Integer> ys = new ArrayList<>();
         List<int[]> pos = new ArrayList<>();
         String line = scan.nextLine();
         while (!line.equals("")) {
@@ -24,8 +22,40 @@ public class Day13 {
             int y = Integer.parseInt(nums[1]);
             width = Math.max(width, x+1);
             height = Math.max(height, y+1);
-            // xs.add(x);
-            // ys.add(y);
+            pos.add(new int[]{x, y});
+            line = scan.nextLine();
+        }
+        char[][] paper = new char[height][width];
+        for (int i=0; i<height; i++) {
+            for (int j=0; j<width; j++) {
+                paper[i][j] = '.';
+            }
+        }
+        for (int[] xy : pos) {
+            paper[xy[1]][xy[0]] = '#';
+        }
+        String folding = scan.nextLine().replace("fold along ", "");
+        String direction = folding.substring(0, 1);
+        int num = Integer.parseInt(folding.substring(2));
+        if (direction.equals("x")) {
+            paper = foldPaperVertically(paper, num);
+        } else {
+            paper = foldPaperHorizontally(paper, num);
+        }
+        // printPaper(paper);
+        System.out.println(countDots(paper));
+    }
+
+    public static void part2(File file) throws FileNotFoundException {
+        Scanner scan = new Scanner(file);
+        List<int[]> pos = new ArrayList<>();
+        String line = scan.nextLine();
+        while (!line.equals("")) {
+            String[] nums = line.split(",");
+            int x = Integer.parseInt(nums[0]);
+            int y = Integer.parseInt(nums[1]);
+            width = Math.max(width, x+1);
+            height = Math.max(height, y+1);
             pos.add(new int[]{x, y});
             line = scan.nextLine();
         }
@@ -47,20 +77,59 @@ public class Day13 {
             String direction = folding.substring(0, 1);
             int num = Integer.parseInt(folding.substring(2));
             if (direction.equals("x")) {
-                paper = foldPaperVertically(paper);
+                paper = foldPaperVertically(paper, 0);
             } else {
-                paper = foldPaperHorizontally(paper);
+                paper = foldPaperHorizontally(paper, 0);
+            }
+            if (height == 13) {
+                print(paper);
             }
         }
+        print(paper);
+        System.out.println(countDots(paper));
     }
 
-    public static char[][] foldPaperVertically(char[][] paper) {
-
-        return null;
+    public static int countDots(char[][] paper) {
+        int count = 0;
+        for (int i=0; i<paper.length; i++) {
+            for (int j=0; j<paper[0].length; j++) {
+                count = paper[i][j] == '.' ? count : count+1;
+            }
+        }
+        return count;
     }
 
-    public static char[][] foldPaperHorizontally(char[][] paper) {
-        
-        return null;
+    public static char[][] foldPaperVertically(char[][] paper, int num) {
+        int offSet = width%2==0 ? 1 : 0;
+        width /= 2;
+        char[][] newPaper = new char[height][width];
+        for (int i=0; i<height; i++) {
+            for (int j=0; j<width; j++) {
+                newPaper[i][j] = paper[i][j] == '#' || paper[i][width*2-j-offSet] == '#' ? '#' : '.';
+            }
+        }
+        return newPaper;
+    }
+
+    public static char[][] foldPaperHorizontally(char[][] paper, int num) {
+        int offSet = height%2==0 ? 1 : 0;
+        height /= 2;
+        char[][] newPaper = new char[height][width];
+        for (int i=0; i<height; i++) {
+            for (int j=0; j<width; j++) {
+                newPaper[i][j] = paper[i][j] == '#' || paper[height*2-i-offSet][j] == '#' ? '#' : '.';
+            }
+        }
+        return newPaper;
+    }
+
+    public static void print(char[][] paper) {
+        for (int i=0; i<paper.length; i++) {
+            for (int j=0; j<paper[0].length; j++) {
+                System.out.print(paper[i][j]);
+            }
+            System.out.println("");
+        }
+        System.out.println("");
     }
 }
